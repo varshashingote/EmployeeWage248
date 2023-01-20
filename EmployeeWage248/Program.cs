8b47177d118c24c75abed90f5f69f2b7d0a5151e
varsha Gunjal <varshashingote01@gmail.com>
2023-01-20 16:19:07 +0530

    [varsha] modified EmployeeWage for month

--- EmployeeWage248/Program.cs
+++ EmployeeWage248/Program.cs
@@ -8,44 +8,59 @@ namespace EmployeeWage248
 {
 
     internal class Program
-    { 
+    {
 
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        
+        public const int NUM_OF_WORKING_DAYS = 2;
+
+
 
         static void Main(string[] args)
         {
-          
-           //variable
+
+            //variable
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-
-            int EmpCheck = random.Next(0, 3);
-            //Computation
-            switch (EmpCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
 
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
 
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+
+                int EmpCheck = random.Next(0, 3);
+                //Computation
+                switch (EmpCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage:" + empWage);
+
+
             }
-            
-                  empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage:"+empWage);
 
+
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
             Console.ReadLine();
-
-
         }
     }
 }
+       
+    
+
+
