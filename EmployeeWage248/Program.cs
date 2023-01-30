29a9a61c1177497435ec72462a02716cb1be6632
varsha Gunjal <varshashingote01@gmail.com>
2023-01-30 18:59:28 +0530

    Add Employee Wage Using OOP

--- EmployeeWage248/Program.cs
+++ EmployeeWage248/Program.cs
@@ -11,15 +11,14 @@ namespace EmployeeWage248
     internal class Program
     {
 
+
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int NUM_OF_WORKING_DAYS = 2;
         public const int MAX_HRS_IN_MONTH = 10;
 
-
-
-        static void Main(string[] args)
+        public static void WageEmployee()
         {
 
             //variable
@@ -56,12 +55,14 @@ namespace EmployeeWage248
             int totalEmpWage = totalEmpHrs + EMP_RATE_PER_HOUR;
             Console.WriteLine("total Employee Wage" + totalEmpWage);
             Console.ReadLine();
-
-
         }
-
+        public static void Main(String[] args)
+        {
+            WageEmployee();
+        }
     }
 }
+            
 
         
 
