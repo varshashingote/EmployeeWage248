23147e8ce775f1c017859a133ec5f7745e5c7de9
varsha Gunjal <varshashingote01@gmail.com>
2023-02-07 13:30:56 +0530

    [varsha] Total Wage by company

--- EmployeeWage248/Program.cs
+++ EmployeeWage248/Program.cs
@@ -10,10 +10,14 @@ namespace EmployeeWage248
     {
         static void Main(String[] args)
         {
+
+
             EmpWageBuilder empWageBuilder = new EmpWageBuilder();
             empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
             empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
             empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for Dmart company :{0} ", empWageBuilder.getTotalWage("Dmart"));
+            Console.WriteLine("Total Wage for Reliance company :{0} ", empWageBuilder.getTotalWage("Reliance"));
             Console.ReadLine();
         }
     }
